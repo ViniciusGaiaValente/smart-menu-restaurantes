e8d62f4eadb167f843af4a5b4e567459a2b0d062
Vinicius Valente <viniciusgaiavalente@gmail.com>
2019-05-08 12:45:44 -0300

    Initial Commit

---
+++ smart-menu-restaurantes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smart-menu-restaurantes
+//
+//  Created by Vinicius Valente on 08/05/19.
+//  Copyright © 2019 Vinicius Valente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
